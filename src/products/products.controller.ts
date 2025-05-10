b5127fb854136bf26f2d129d41e369ef4cdb53a9
Steven Bossio Leal <bossiolealbl@gmail.com>
2025-05-10 12:40:50 -0500

    Adds product creation and update features
    Implements product creation and update functionalities in the products microservice via DTOs and controller methods. Also, adds product delete feature.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -14,8 +16,11 @@ export class ProductsController {
   ) {}
 
   @Post()
-  createProduct(){
-    return 'crea'
+  async createProduct(@Body() createProduct: CreateProductDto){
+    return this.productsClient.send({cmd: 'create_product'},createProduct)
+    .pipe(
+      catchError(err => {throw new RpcException(err)})
+    )
   }
 
   @Get()
@@ -33,12 +38,24 @@ export class ProductsController {
 
   @Delete(':id')
   deleteProduct(@Param('id') id: string){
-    return 'elimina el producto ' + id
+    return this.productsClient.send({cmd: 'delete_product'}, {id})
+    .pipe(
+      catchError(err => {throw new RpcException(err)})
+    )
   }
 
   @Patch(':id')
-  updateProduct(@Param('id') id: string){
-    return 'Actualiza el producto ' + id
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProduct: UpdateProductDto
+  ){
+    return this.productsClient.send({cmd: 'update_product'}, {
+      id,
+      ...updateProduct
+    })
+    .pipe(
+      catchError(err => {throw new RpcException(err)})
+    )
   }
 
   
